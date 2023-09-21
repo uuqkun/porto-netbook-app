61cc911cab468e19409978a735d8ce48801ebb88
Achmad Furqon Rachmadie <knowledge.oz2704@gmail.com>
2023-09-21 20:24:46 +0700

    button animation

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,15 @@
+import { easeInOut, motion } from "framer-motion";
+
 const Button = ({ value, classes, event: onClick }) => (
-  <button
+  <motion.button
     type="button"
     className={`py-4 px-7 text-[12px] md:text-[16px] tracking-wide font-bold font-quicksand rounded-xl outline-none w-auto transition ease-in-out hover:scale-110 ${classes}`}
     onClick={onClick}
+    whileInView={{ opacity: [0.5, 1] }}
+    transition={{ duration: 0.5 }}
   >
     {value}
-  </button>
+  </motion.button>
 );
 
 export default Button;
